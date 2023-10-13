e929f8811089d6120edca45d76d69a8bf7aff21b
nestaraus <nestaraus@mail.pu>
2023-10-13 19:57:33 +0300

    zadacha2

---
+++ 1.1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите на вход три числа и проверьте может ли существовать треугольник со сторонами такой длинны.
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"[{string.Join(", ", size)}]");
+if (size[0]<size[1]+size[2] &&
+    size[1]<size[2]+size[0] &&
+    size[2]<size[1]+size[0])
+Console.Write("yes");
+else
+Console.Write("no");
