e2e62430e8b651c70bfc087460cc3f4d85b3840a
nestaraus <nestaraus@mail.pu>
2023-10-13 20:42:28 +0300

    zadacha5

---
+++ 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая создает копию заданного массива с помощью поэлементного копирования
+
+
+void copy(int[] arr, int[] newArr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i]=arr[i];
+
+    }
+}
+
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[] newArr = new int [array.Length];
+copy(array,newArr);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", newArr)}]");
