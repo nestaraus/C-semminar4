4c7f6fbed508a8bbc3e8826bf536957a86db3994
nestaraus <nestaraus@mail.pu>
2023-10-13 20:14:12 +0300

    zadacha3

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n>0)
+{
+    result = Convert.ToString(n%2)+ result;
+    n/=2;
+}
+Console.Write(result);
